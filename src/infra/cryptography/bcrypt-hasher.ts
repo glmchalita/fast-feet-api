2b5ae5682f8c3d22ae9a60e39965f70395871d12
Guilherme Chalita <guilhermechalita@hotmail.com>
2025-01-10 00:36:44 -0300

    refactor: Semantic changes

--- src/infra/cryptography/bcrypt-hasher.ts
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -5,11 +5,11 @@ import { compare, hash } from 'bcryptjs'
 export class BcryptHasher implements HashGenerator, HashComparator {
   private HASH_SALT_LENGTH = 8
 
-  async hash(plain: string): Promise<string> {
+  hash(plain: string): Promise<string> {
     return hash(plain, this.HASH_SALT_LENGTH)
   }
 
-  async compare(plain: string, hash: string): Promise<boolean> {
+  compare(plain: string, hash: string): Promise<boolean> {
     return compare(plain, hash)
   }
 }
--- src/infra/cryptography/cryptography.module.ts
+++ src/infra/cryptography/cryptography.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
+
 import { JwtEncrypter } from './jwt-encrypter'
 import { BcryptHasher } from './bcrypt-hasher'
+
 import { Encrypter } from '@/domain/delivery/application/cryptography/encrypter'
 import { HashComparator } from '@/domain/delivery/application/cryptography/hash-comparator'
 import { HashGenerator } from '@/domain/delivery/application/cryptography/hash-generator'
